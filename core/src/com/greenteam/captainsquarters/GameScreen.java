def9ad3d6ede0249c0f6f6ce790dbcad5d53a03b
Yoshi <forbes.y2403@gmail.com>
2024-07-25 11:38:08 +0100

    touch input added

--- core/src/com/greenteam/captainsquarters/GameScreen.java
+++ core/src/com/greenteam/captainsquarters/GameScreen.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -38,6 +39,7 @@ public class GameScreen implements Screen {
     //parameters for world
     private final int WORLD_WIDTH = 72;
     private final int WORLD_HEIGHT = 128;
+    private final float TOUCH_MOVEMENT_THRESHOLD = 0.5f;
 
     //game objects
     private Ship playerShip;
@@ -172,6 +174,48 @@ public class GameScreen implements Screen {
             playerShip.translate(0f, Math.max(-playerShip.movementSpeed*deltaTime, leftLimit));
         }
         //touch input (applies for mouse)
+        if(Gdx.input.isTouched()){
+            //get screen position of touched location
+            float xTouchPixels = Gdx.input.getX();
+            float yTouchPixels = Gdx.input.getY();
+
+            //convert to world position
+            Vector2 touchPoint = new Vector2(xTouchPixels, yTouchPixels);
+            touchPoint = viewport.unproject(touchPoint);
+
+            //calculate x/y difference
+            Vector2 playerShipCentre = new Vector2(
+                    playerShip.boundingBox.x + playerShip.boundingBox.width,
+                    playerShip.boundingBox.y + playerShip.boundingBox.height);
+
+            float touchDistance = touchPoint.dst(playerShipCentre);
+
+            if(touchDistance > TOUCH_MOVEMENT_THRESHOLD){
+                float xTouchDifference = touchPoint.x - playerShipCentre.x;
+                float yTouchDifference = touchPoint.y - playerShipCentre.y;
+
+                //scale to max speed of the ship
+                float xMove = xTouchDifference / touchDistance * playerShip.movementSpeed * deltaTime;
+                float yMove = yTouchDifference / touchDistance * playerShip.movementSpeed * deltaTime;
+
+                if(xMove > 0) xMove = Math.min(xMove, rightLimit);
+                else xMove = Math.max(xMove, leftLimit);
+
+                if(yMove > 0) yMove = Math.min(yMove, upperLimit);
+                else yMove = Math.max(yMove, lowerLimit);
+
+                playerShip.translate(xMove, yMove);
+
+
+
+            }
+
+
+
+
+        }
+
+
     }
 
     private void detectCollisions(){
