220a880f274a822b002aa0bdc32ab9238b8975a8
Yoshiwa1337 <forbes.y2403@gmail.com>
2024-07-30 08:49:41 +0100

    added backbtn to decryption page

--- android/src/com/greenteam/captainsquarters/Decryption.java
+++ android/src/com/greenteam/captainsquarters/Decryption.java
@@ -1,24 +1,45 @@
 package com.greenteam.captainsquarters;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Decryption extends AppCompatActivity {
+    ImageView backBtn;
+    ConstraintLayout btnContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_decryption);
+        LayoutInflater inflater = getLayoutInflater();
+        View vv = inflater.inflate(R.layout.back_btn,null);
+        backBtn = vv.findViewById(R.id.back_btn);
+        btnContainer = (ConstraintLayout)findViewById(R.id.btnContainer);
+        btnContainer.addView(vv);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Home_Page.class);
+                startActivity(intent);
+            }
         });
     }
 }
