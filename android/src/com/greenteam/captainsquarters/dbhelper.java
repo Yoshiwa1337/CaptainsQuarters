55067bcf620dcddc6460ce14cbf8b4351615e49c
hadigun02 <103096uk@saeinstitute.edu>
2024-08-09 16:43:03 +0100

    final database changes

--- android/src/com/greenteam/captainsquarters/dbhelper.java
+++ android/src/com/greenteam/captainsquarters/dbhelper.java
@@ -12,7 +12,7 @@ public class dbhelper extends SQLiteOpenHelper {
 
     private static String DBname = "lone";
     private static String DBtable = "users";
-    private static int DBversion = 1;
+    private static int DBversion = 3;
 
     private static String ID = "Id";
     private static String userName = "username";
